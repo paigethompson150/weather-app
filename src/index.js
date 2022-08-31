ac23494e83be4248c3a9e032dc908d9cc3f93bca
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2022-08-31 00:01:29 -0400

    populating data works

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import loadJson from './loadJson.js';
 import populateData from './populateData.js';
 
-let search = 'Boston';
+let search = 'London';
 let unit = 'imperial';
 
 
 //retrieve array of data from weather API
-let weather = loadJson(search, unit).then(function(response) {
-  console.log(response);
+loadJson(search, unit).then(function(response) {
+  populateData(response);
 }, function(error) {
   console.log('failed', error);
 });
 
-populateData(weather);
+
 
--- src/populateData.js
+++ src/populateData.js
@@ -1,5 +1,37 @@
 //target DOM elements and populate weather data into DOM
 function populateData(data){
+  //temperature display in DOM
+  let temp = document.getElementById('temp');
+  //city display in DOM
+  let city = document.getElementById('city');
+  //country display in DOM
+  let country = document.getElementById('country');
+  //description display
+  let description = document.getElementById('description');
+  //feels like display
+  let feelsLike = document.getElementById('feelsLike');
+  //max temp display
+  let maxTemp = document.getElementById('maxTemp');
+  //min temp display
+  let minTemp = document.getElementById('minTemp');
+  //humidity display
+  let humidity = document.getElementById('humidity');
+  //wind display
+  let wind = document.getElementById('wind');
+
+
+  console.log(data);
+
+  //appending items to DOM
+  temp.innerHTML = data.temp;
+  city.innerHTML = data.name;
+  country.innerHTML = data.country;
+  description.innerHTML = data.description;
+  feelsLike.innerHTML = data.feelsLike;
+  maxTemp.innerHTML = data.tempMax; 
+  minTemp.innerHTML = data.tempMin; 
+  humidity.innerHTML = data.humidity; 
+  wind.innerHTML = data.wind;
 
 }
 
